4d3002474222c00f4a82d3ba19ef2687fe79ed77
ps-cd <45522676+ps-cd@users.noreply.github.com>
2025-01-03 18:36:07 -0500

    `fn_lib_macros` - print macro-generated tokens as an optional feature for debugging

--- src/fn_lib_tools/macros/src/lib.rs
+++ src/fn_lib_tools/macros/src/lib.rs
@@ -20,23 +20,26 @@ pub fn usrlib_boilerplate(_item: TokenStream) -> TokenStream {
             }
         }
     };
-    // println!("output_tokens: \n{}", output_tokens.clone());
+    // println!("output_tokens: \n{}", output_tokens2.clone());
     TokenStream::from(output_tokens2)
 }
 
 fn lib_fn_macro_base(target_lib: &str, samp_type: &str, attr_tokens: TokenStream, input_tokens: TokenStream) -> TokenStream {
     let input_tokens2 = TokenStream2::from(input_tokens.clone());
 
-    println!("\n======================\n======================\n======================");
+    if cfg!(feature = "debug_token_print") {
+        println!("\n======================================================================");
+    }
     // println!("attr_tokens = {:#?}", attr_tokens);
     // println!("input_tokens = {:#?}", input_tokens);
 
     let parsed_struct = syn::parse_macro_input!(input_tokens as syn::ItemStruct);
-
-    // println!("parsed_input = {:#?}", parsed_struct);
+    // println!("parsed_struct = {parsed_struct:#?}");
 
     let struct_ident = parsed_struct.ident.clone();
-    println!("struct_ident = {struct_ident}");
+    if cfg!(feature = "debug_token_print") {
+        println!("struct_ident = {struct_ident}");
+    }
 
     let mut doc_tokens = TokenStream2::new();
     for attr_item in parsed_struct.attrs.iter() {
@@ -54,27 +57,9 @@ fn lib_fn_macro_base(target_lib: &str, samp_type: &str, attr_tokens: TokenStream
 
         field_idents.push(ident_);
         field_ident_ty_tokens.push(ident_ty_tokens);
-
-        // let field_name = field.ident.clone().expect("All struct fields must have a name. Unnamed fields are not supported");
-        //
-        // let field_type = match field.ty.clone() {
-        //     syn::Type::Path(type_path) => {
-        //         type_path.path.segments.last().unwrap().ident.clone()
-        //     },
-        //     _ => panic!("Struct field {} has unsupported type", field_name.to_string())
-        // };
-        // println!("field_name = {field_name:?}");
-        // println!("field_type = {field_type:?}");
-        //
-        // let x = field.to_token_stream();
-        // println!("x = {}", x);
-        // println!("---")
     }
     // println!("field_idents = {field_idents:#?}");
-    // // println!("field_ident_type_tokens = {field_ident_type_tokens:#?}");
-    // for field_token_stream in field_ident_type_tokens.iter() {
-    //     println!("field_token_stream = {field_token_stream}")
-    // };
+    // println!("field_ident_ty_tokens = {field_ident_ty_tokens:#?}");
 
     let impl_pub_fn_new_tokens = quote!{
         impl #struct_ident {
@@ -83,18 +68,14 @@ fn lib_fn_macro_base(target_lib: &str, samp_type: &str, attr_tokens: TokenStream
             }
         }
     };
-    // println!("impl_pub_fn_new_tokens: \n{}\n", impl_pub_fn_new_tokens);
-
-    // println!("attr_tokens = {}", attr_tokens);
-    // println!("attr_tokens.is_empty = {}", attr_tokens.is_empty());
-    // let x = attr_tokens.is_empty();
+    // println!("impl_pub_fn_new_tokens: \n{impl_pub_fn_new_tokens}\n");
 
     let pyo3_sig_tokens = if attr_tokens.is_empty() {
         quote!{#(#field_idents),*}
     } else {
         TokenStream2::from(attr_tokens)
     };
-    // println!("pyo3_sig_tokens: \n{}\n", pyo3_sig_tokens);
+    // println!("pyo3_sig_tokens: \n{pyo3_sig_tokens}\n");
 
     let target_lib_tokens = TokenStream2::from_str(target_lib).unwrap();
     let fn_box_tokens = TokenStream2::from_str(
@@ -116,7 +97,7 @@ fn lib_fn_macro_base(target_lib: &str, samp_type: &str, attr_tokens: TokenStream
             }
         }
     };
-    // println!("pymethods_impl_target_lib_tokens: \n{}\n", pymethods_impl_target_lib_tokens);
+    // println!("pymethods_impl_target_lib_tokens: \n{pymethods_impl_target_lib_tokens}\n");
 
     let full_tokens = quote!{
         #[derive(Clone, Debug)]
@@ -126,7 +107,9 @@ fn lib_fn_macro_base(target_lib: &str, samp_type: &str, attr_tokens: TokenStream
 
         #pymethods_impl_target_lib_tokens
     };
-    println!("full_tokens: \n{}\n", full_tokens);
+    if cfg!(feature = "debug_token_print") {
+        println!("full_tokens: \n{}\n", full_tokens);
+    }
 
     // TokenStream::from(input_tokens2)
     TokenStream::from(full_tokens)
